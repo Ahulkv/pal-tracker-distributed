24ebfd88b4d090f821e2f00b25ef139c857ab02a
ashu8tosh <ashutosh3089@gmail.com>
2019-11-21 15:52:19 +0530

    Circuit-breaker

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -5,6 +5,7 @@ import io.pivotal.pal.tracker.allocations.ProjectClient;
 import org.springframework.beans.factory.annotation.Value;
         import org.springframework.boot.SpringApplication;
         import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
         import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
         import org.springframework.context.annotation.Bean;
         import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Value;
 
         import java.util.TimeZone;
 
+
+@EnableCircuitBreaker
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.allocations", "io.pivotal.pal.tracker.restsupport"})
